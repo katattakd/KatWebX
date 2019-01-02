0a4a04387fc2ebd388c5b419f5d8a544de3556ae
Kat <kittyhacker101@protonmail.com>
2019-01-02 15:03:37 -0800

    Use Appveyor for release packaging, minor fixes

--- ci/script.sh
+++ ci/script.sh
@@ -1,20 +1,17 @@
 set -ex
 
 main() {
-	cross build --target $TARGET
 	cross build --target $TARGET --release
 
 	if [ ! -z $DISABLE_TESTS ]; then
 		return
 	fi
 
-	cross test --target $TARGET
 	cross test --target $TARGET --release
 
+	# Clippy doesn't currently work with cross-compiling: https://github.com/rust-embedded/cross/issues/176
 	rustup component add clippy-preview
-
-	cross clippy --target $TARGET
-	cross clippy --target $TARGET --release
+	cargo clippy --target $TARGET --release
 }
 
 if [ -z $TRAVIS_TAG ]; then
