5653ca90521f633b9d1a8e2d3c7a3afd7042296c
Kat <kittyhacker101@protonmail.com>
2019-01-05 20:04:00 -0800

    Fixed Travis CI x86_64 builds

--- ci/script.sh
+++ ci/script.sh
@@ -1,11 +1,6 @@
 set -ex
 
 main() {
-	if [ $TARGET == 'x86_64-unknown-linux-musl' ]; then
-		rustup component add clippy-preview
-		cargo clippy --target $TARGET --release
-	fi
-
 	cross build --target $TARGET --release
 
 	if [ ! -z $DISABLE_TESTS ]; then
@@ -15,8 +10,10 @@ main() {
 	cross test --target $TARGET --release
 
 	# Clippy doesn't currently work with cross-compiling: https://github.com/rust-embedded/cross/issues/176
-	#rustup component add clippy-preview
-	#cross clippy --target $TARGET --release
+	if [ $TARGET == 'x86_64-unknown-linux-musl' ]; then
+		rustup component add clippy-preview
+		cross clippy --target $TARGET --release
+	fi
 }
 
 if [ -z $TRAVIS_TAG ]; then
