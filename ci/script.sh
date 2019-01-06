64759dda824dc635df62e3ac5295a8ca55d706b9
Kat <kittyhacker101@protonmail.com>
2019-01-05 20:23:17 -0800

    Disabled clippy for Travis CI builds

--- ci/script.sh
+++ ci/script.sh
@@ -10,10 +10,8 @@ main() {
 	cross test --target $TARGET --release
 
 	# Clippy doesn't currently work with cross-compiling: https://github.com/rust-embedded/cross/issues/176
-	if [ $TARGET == 'x86_64-unknown-linux-musl' ]; then
-		rustup component add clippy-preview
-		cross clippy --target $TARGET --release
-	fi
+	#rustup component add clippy-preview
+	#cross clippy --target $TARGET --release
 }
 
 if [ -z $TRAVIS_TAG ]; then
