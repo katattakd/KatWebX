1c8e4b2e8a3505d344b80a10c6258b2fd7591ec3
Kat <kittyhacker101@protonmail.com>
2019-01-31 21:02:20 -0800

    Improved release packaging

--- ci/before_deploy.ps1
+++ ci/before_deploy.ps1
@@ -8,9 +8,9 @@ Set-Location $STAGE
 $ZIP = "$SRC_DIR\$($Env:CRATE_NAME)-$($Env:APPVEYOR_REPO_TAG_NAME)-$($Env:TARGET).zip"
 
 Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\katwebx.exe" '.\'
-Copy-Item "$SRC_DIR\html\*" '.\' -recurse
-Copy-Item "$SRC_DIR\src\*" '.\' -recurse
-Copy-Item "$SRC_DIR\ssl\*" '.\' -recurse
+Copy-Item "$SRC_DIR\html\*" '.\html\' -recurse
+Copy-Item "$SRC_DIR\src\*" '.\src\' -recurse
+Copy-Item "$SRC_DIR\ssl\*" '.\ssl\' -recurse
 
 7z a "$ZIP" *
 
